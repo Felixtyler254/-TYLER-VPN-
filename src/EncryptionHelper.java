d6d38a2486a22653bd6583c76047b44ffd562f8e
Tyler <felixtyler530@gmail.com>
2024-12-19 15:49:59 +0300

    New modules

---
+++ src/EncryptionHelper.java
@@ -0,0 +1,24 @@
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+public class EncryptionHelper {
+    private static final String ALGORITHM = "AES";
+    private static final String KEY = "1234567890123456"; // 16-byte key
+
+    public static String encrypt(String data) throws Exception {
+        SecretKeySpec key = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    public static String decrypt(String encryptedData) throws Exception {
+        SecretKeySpec key = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] original = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+        return new String(original);
+    }
+}
